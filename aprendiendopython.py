a7f511a8fa25eb470fe47705fe6dd108d3ce655e
robertoperez2793 <robertoperez2793@gmail.com>
2022-05-03 23:06:26 -0300

    Comienzo del proyecto

---
+++ aprendiendopython.py
@@ -0,0 +1,11 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> numero1=5
+>>> numero2=10
+>>> if numero1>numero2:
+... 	print("El numero 1 es mayor")
+... else:
+... 	print ("El numero 2 es mayor")
+... 
+El numero 2 es mayor
+>>> 
---
+++ pythonconsublime.py
@@ -0,0 +1,9 @@
+def mensaje():
+
+	print ("Estamos aprendiendo Python")
+	print ("Python es lo mejor que hay")
+	print ("poco a poco iremos cumpliendo los objetivos")
+
+mensaje()
+mensaje()
+mensaje()
